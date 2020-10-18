a77f43c25485a846c2cf766bc4238488539d3c14
Muriel Jakazi <murielnjakazi@gmail.com>
2020-10-18 23:09:26 +0200

    chat-app with modifiable online status

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import './Contact.css';
 
+class Contact extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: this.props.online
+    };
+  }
 
-function Contact(props) {
+  render() {
+    console.log(this.state.online)
     return (
       <div className="Contact">
-        <img className="avatar" src={props.avatar} alt={props.name} />
+        <img className="avatar" src={this.props.avatar} alt={this.props.name} />
         <div>
-          <p className="name">{props.name}</p>
-          <div className="status">
-            <div className={props.online ? "status-online" : "status-offline"} />
-            <p className="status-text">{props.online ? "online" : "offline"}</p>
+          <p className="name">{this.props.name}</p>
+          <div 
+           onClick={event => {
+            //  const newStatus = this.state.online;
+             this.setState({ online : !this.state.online});
+           }}
+          className="status">
+            <div className={this.state.online ? "status-online" : "status-offline"} />
+            <p className="status-text">{this.state.online ? "online" : "offline"}</p>
           </div>
         </div>
       </div>
     );
   }
+}
+
 
 export default Contact;
